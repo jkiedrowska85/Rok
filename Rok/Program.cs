08b8ca45eb60b94d86557ce432ccf021612c92b7
jkiedrowska85 <jkiedrowska85@gmail.com>
2022-08-19 16:33:38 +0200

    Add project files.

---
+++ Rok/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Rok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = Convert.ToInt32(Console.ReadLine());
+
+            if (i % 4 == 0)
+            {
+                Console.WriteLine("Rok przestępny");
+            }
+            else
+            if (i % 100 == 0 && i <= 100 )
+            {
+                Console.WriteLine("Rok przestępny");
+            }
+            else
+            if (i % 400 == 0 && i <= 400)
+            {
+                Console.WriteLine(" Rok przestępny");
+            }
+            else
+                Console.WriteLine(" Rok nieprzestępny");
+        }
+    }
+}
